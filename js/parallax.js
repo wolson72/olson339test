59182b8c87ee9a64624edcbe8d995063fc0550f8
wolson72 <38336317+wolson72@users.noreply.github.com>
2020-05-31 14:13:27 -0700

    Add files via upload

---
+++ js/parallax.js
@@ -0,0 +1,13 @@
+
+$(window).scroll(function () {
+var wScroll = $(this).scrollTop();
+
+$(".title").css({
+transform: "translate(0px, " + wScroll / 1.5 + "%)"
+});
+
+$(".transparent").css({
+transform: "translate(0px, -" + wScroll / 3 + "%)"
+});
+
+});
